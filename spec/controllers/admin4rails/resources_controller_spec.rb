367dc043699369b4f912a2ed54d7987478403d73
MirkoMignini <mirko.mignini@gmail.com>
2016-03-28 12:20:44 +0200

    Fixed rspec

--- spec/controllers/admin4rails/resources_controller_spec.rb
+++ spec/controllers/admin4rails/resources_controller_spec.rb
@@ -2,5 +2,20 @@ require 'rails_helper'
 
 module Admin4rails
   RSpec.describe ResourcesController, type: :controller do
+    routes {Admin4rails::Engine.routes}
+
+    describe 'Dynamic controller creation' do
+      it 'Creates the controller' do
+        expect(Admin4rails.const_get('PostsController')).to be(Admin4rails::PostsController)
+      end
+    end
+
+    describe "routing" do
+      it "routes to index" do
+        expect(get: "/posts").to route_to(controller: 'admin4rails/posts', action: 'index')
+        expect(get: "/posts.json").to route_to(controller: 'admin4rails/posts', action: 'index', format: 'json')
+      end
+    end
+
   end
 end
