98db90dd7b2c6439aa24cbbfdbd0d6cec7fc1ddc
Mirko Mignini <mirko.mignini@gmail.com>
2016-03-31 14:23:39 +0200

    Changed setup sequence

---
+++ config/initializers/default.rb
@@ -0,0 +1,30 @@
+Admin4rails.setup do
+  config! do
+    title! 'Admin4Rails'
+    adapter! :activerecord
+  end
+
+  header! do
+    visible! true
+    logo! do
+      text! '<strong>Admin</strong>4<i>Rails</i>'
+      text_mini! '<strong>A</strong>4<i>R</i>'
+      href! '#'
+    end
+  end
+
+  sidebar do
+    layout! do
+      header text: 'Main'
+      resources
+    end
+  end
+
+  resource class: Post do
+    icon! 'fa-table'
+  end
+
+  resource class: Comment
+end
+
+Admin4rails.initialize!
