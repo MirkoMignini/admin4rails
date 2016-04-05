4cca06920a8ebff57faaffedde5615c76773f751
Mirko Mignini <mirko.mignini@gmail.com>
2016-04-05 18:00:20 +0200

    Custom devise layout

--- lib/admin4rails/engine.rb
+++ lib/admin4rails/engine.rb
@@ -5,5 +5,9 @@ module Admin4rails
     config.generators do |g|
       g.test_framework :rspec
     end
+
+    config.to_prepare do
+      #Devise::SessionsController.layout 'devise'
+    end
   end
 end
